103e92927d29c5feb9310307ed1656d8cf4904f5
Lenovo <Lenovo@NANI-0403>
2025-03-08 13:54:08 +0530

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+{
+    Console.WriteLine("Enter 10 numbers");
+    int[] array= new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    int res = int.MaxValue;
+    for (int i = 0; i < 10; i++)
+    {
+        if (array[i] < res)
+        {
+            res = array[i];
+        }
+    }
+    Console.WriteLine(res);
+
+} 
